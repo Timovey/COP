45f7345aeab8fecae7bda6041c94c8eb01aca1c6
Timofey Kochetkov <pochta30052001@gmail.com>
2021-09-15 23:37:06 +0400

    add 3

---
+++ WinFormsLibrary/UserControl3.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace WinFormsLibrary
+{
+    partial class UserControl3
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 15;
+            this.ListBox.Location = new System.Drawing.Point(74, 38);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(213, 274);
+            this.ListBox.TabIndex = 0;
+            // 
+            // UserControl3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ListBox);
+            this.Name = "UserControl3";
+            this.Size = new System.Drawing.Size(632, 479);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
---
+++ WinFormsLibrary/UserControl3.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsLibrary
+{
+    public partial class UserControl3 : UserControl
+    {
+        private string Field1 = "";
+        private string Field2 = "";
+        private bool isField = false;
+        private string template = "";
+
+        public void AddTemplate(string templateFromOutside, char begin, char end)
+        {
+            string[] splitedStrings = templateFromOutside.Split(begin, end);
+            if(splitedStrings.Length <3)
+            {
+                return;
+            }
+            Field1 = splitedStrings[1];
+            Field2 = splitedStrings[2];
+            isField = true;
+            for(int i = 0; i < splitedStrings.Length;i++)
+            {
+                
+                if(i == 1)
+                {
+                    template += $"{1}";
+                }
+                if(i == 3)
+                {
+                    template += $"{2}";
+                }
+                template += splitedStrings[i];
+            }
+        }
+        public UserControl3()
+        {
+            InitializeComponent();
+        }
+
+        public void AddToList<T>(ref T obj, int number, string Name)
+        {
+            if(!isField && !Field1.Equals("") && !Field2.Equals(""))
+            {
+                throw new Exception("Поля не заполнены");
+            }
+            string o1;
+            string o2;
+            Type type = obj.GetType();
+            PropertyInfo[] props = type.GetProperties();
+            FieldInfo[] fields = type.GetFields();
+            foreach(var p in props)
+            {
+                if(p.Name.Equals(Field1))
+                {
+                    o1 = p.GetValue(null).ToString();
+                }
+                if (p.Name.Equals(Field2))
+                {
+                    o2 = p.GetValue(null).ToString();
+                }
+            }
+
+            foreach (var f in fields)
+            {
+                if (f.Name.Equals(Field1))
+                {
+                    o1 = f.GetValue(null).ToString();
+                }
+                if (f.Name.Equals(Field2))
+                {
+                    o2 = f.GetValue(null).ToString();
+                }
+            }
+
+
+        }
+    }
+}
